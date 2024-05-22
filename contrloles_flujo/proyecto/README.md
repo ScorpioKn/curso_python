af675200888d850688d8e5164b880ce55912bac4
ScorpioKn <scorpio646asd@gmail.com>
2024-05-22 10:13:21 -0500

    actualice la tarea

---
+++ contrloles_flujo/proyecto/README.md
@@ -0,0 +1,5 @@
+#el usuario tiene un grass el cual administra de manera manual, el usuario necesita que se automatize el proceso de la reserva y el pago del alquiler, para lo cual solicita los siguientes casos de uso.
+el usuario podra ver la lista de horarios disponibles.
+el usuario podria reservar en uno de los horarios disponibles.
+el usuario podra pagar por el alquiler de la reserva realizada.
+el usuario podra verificar su alquiler el cual le mostrara los detalles como la fecha, hora y costo del alquiler que realizo.
---
+++ contrloles_flujo/proyecto/mayn.py
@@ -0,0 +1,18 @@
+horarios_disponibles=["06:00- 08:00","13:00-15:00","16:00-18:00","19:00-21:00"]
+print("horas_disponibles: ")
+for horarios in horarios_disponibles:
+    print(horarios)
+horarios_seleccionada=input("selecione un horario de la lista: ")
+reserva_lista=True
+if reserva_lista:
+    print("reserva realizada con exito.")
+    costo_alquiler=10
+    pago_realizado= True
+    if pago_realizado:
+        print("pago del alquiler realizado.")
+        print("detalles del alquiler.")
+        print(f"fecha y hora: {horarios_seleccionada}")
+        print(f"costo: $ {costo_alquiler}")
+    else:
+        print("error en el pago del alquiler")
+        print("error en la reserva del horario.")
